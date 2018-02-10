182e54307f4d42fa2360138df81288418218f082
thatruth <thankgodegbo@gmail.com>
2018-02-11 00:05:53 +0100

    Added the php file.

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+function encrypt() { 
+	$index = range('a', 'z');
+
+
+if  (isset($_POST['string']) &&!empty($_POST['string'])) {
+
+	$string = $_POST['string'];
+
+	$string_split = str_split($string); // converts the string into an array
+
+	end($string_split); 
+       $key_index = key($string_split); // gets the key of the last item in the string
+
+ 
+
+for ($i=0; $i <= $key_index; $i++) { 
+		$search = array_search($string_split[$i], $index);
+if ( $search % 2 == 0 ) {
+		$search += 1;
+		$odd = $search * 3;
+		$odd_final = $odd + 1;
+		$search = 'o'.$odd_final;
+		} else {
+if ($search % 2 == 1) {
+		$search += 1;
+		$even = $search / 2;
+	 	$search='e'.$even;
+		
+	 }
+		}
+				
+	 		print_r($search);
+	}
+
+}
+
+}
+encrypt();
+
+?>
+
+	<form action='index.php' method='POST'>
+	<input type = 'text' name='string'>
+	<input type = 'submit' value = 'submit'>
+	</form>
