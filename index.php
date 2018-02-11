8a4a191af1267ceac66837445b5cf9c0db93f03f
thatruth <thankgodegbo@gmail.com>
2018-02-11 23:56:07 +0100

    Added Some Styles.

--- index.php
+++ index.php
@@ -18,14 +18,14 @@ if  (isset($_POST['string']) &&!empty($_POST['string'])) {
  
 
 for ($i=0; $i <= $key_index; $i++) { 
-		$search = array_search($string_split[$i], $index);
-if ( $search % 2 == 0 ) {
+		$search = array_search($string_split[$i], $index); // search for each letter in the array
+if ( $search % 2 == 0 ) { // condition for odd values
 		$search += 1;
 		$odd = $search * 3;
 		$odd_final = $odd + 1;
 		$search = 'o'.$odd_final;
 		} else {
-if ($search % 2 == 1) {
+if ($search % 2 == 1) { // condition for even values
 		$search += 1;
 		$even = $search / 2;
 	 	$search='e'.$even;
@@ -42,8 +42,8 @@ if ($search % 2 == 1) {
 encrypt();
 
 ?>
-
-	<form action='index.php' method='POST'>
-	<input type = 'text' name='string'>
-	<input type = 'submit' value = 'submit'>
+	<h2 style="padding-left : 40%; padding-top : 10%;">Enter Your String Here</h2>
+	<form action='index.php' method='POST' style="padding-left : 40%;">
+	<input type = 'text' name='string' style="height : 10%; font-size:120%; font-family:courier;">
+	<input type = 'submit' value = 'Encrypt' style="height : 10%; background-color : red; color : black; font-size:120%; font-family:courier;">
 	</form>
